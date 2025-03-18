3f5338668e3d57c8ee9080e9295534316a08cc38
anup199 <anupsingh6625@gmail.com>
2025-03-18 22:02:50 +0530

    feature added

--- action.js
+++ action.js
@@ -1,15 +1,33 @@
 function makeBold() {
      let textArea = document.getElementById("textArea");
-     // textArea.style.fontWeight = textArea.style.fontWeight === "bold" ? "normal" : "bold";
-     textArea.style.fontWeight = "bold";
+     textArea.style.fontWeight = textArea.style.fontWeight === "bold" ? "normal" : "bold";
 }
+-
 
-function makeItalic() {
-     let textArea = document.getElementById("textArea");
-     textArea.style.fontStyle = textArea.style.fontStyle === "italic" ? "normal" : "italic";
-}
+     function makeItalic() {
+          let textArea = document.getElementById("textArea");
+          textArea.style.fontStyle = textArea.style.fontStyle === "italic" ? "normal" : "italic";
+     }
 
 function makeUnderline() {
      let textArea = document.getElementById("textArea");
      textArea.style.textDecoration = textArea.style.textDecoration === "underline" ? "none" : "underline";
+}
+
+let fontSize = 12;
+function makefontSizebig() {
+     fontSize += 1;
+     let textArea = document.getElementById("textArea");
+     textArea.style.fontSize = fontSize + "px";
+}
+
+// let fontsize = 12;
+function makefontSizesmall() {
+     fontSize -= 1;
+     let textArea = document.getElementById("textArea");
+     textArea.style.fontSize = fontSize + "px";
+}
+function clear() {
+     let textArea = document.getElementById("textArea");
+     textArea.value = "";
 }
